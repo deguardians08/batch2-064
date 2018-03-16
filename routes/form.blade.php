c05e1d1fc8b144269d040993bf188c0b798add3e
kuno.JR <fire11984@gmail.com>
2018-03-17 04:53:21 +0700

    awal

---
+++ routes/form.blade.php
@@ -0,0 +1,25 @@
+@extends('template')
+
+@section('content')
+<div class="form-group">
+    <label>  label</label>
+    <input type="text" name="" id="" class="form-control">
+    </div>
+<div class="form-group">
+    <label>  label</label>
+    <input type="text" name="" id="" class="form-control">
+ </div>
+    <div class="form-group">
+    <label>  label</label>
+    <input type="text" name="" id="" class="form-control">
+</div><div class="form-group">
+    <label>  label</label>
+    <input type="text" name="" id="" class="form-control">
+ </div>
+    <div class="form-group">
+    <label>  label</label>
+    <input type="text" name="" id="" class="form-control">
+        <a herf="{{url('/')}}" class ="btn btn-success"> Simpan Data</a> 
+    </div>
+
+    @endsection
